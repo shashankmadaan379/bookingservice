899f8ec997359591a7630cad99b42d378e4ab5a2
Shashank <codebreaker379@gmail.com>
2023-10-07 16:42:01 +0530

    modify api routes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const db = require("./models/index");
 const setUpAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use("/api", apiRoutes);
+  app.use("/bookingservice/api", apiRoutes);
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,11 @@ const { BookingController } = require("../../controllers/index");
 // const channel=await createChannel();
 const bookingController = new BookingController()
 const router = express.Router();
+router.get("/info",(req,res)=>{
+    return res.json({
+        message:"Response from routes"
+    })
+})
 router.post("/bookings",bookingController.create);
 router.post("/publish",bookingController.sendMessageToQueue)
 module.exports = router;
